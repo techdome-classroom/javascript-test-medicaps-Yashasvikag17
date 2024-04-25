bf2c845cb4bd8596d65c8205129f9df8b02eb2bb
Yashasvikag17 <123310182+Yashasvikag17@users.noreply.github.com>
2024-04-25 13:55:43 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,17 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let maxLength = 0;
+    const charIndex = new Array(128).fill(-1);
+    let left = 0;
+    
+    for (let right = 0; right < s.length; right++) {
+        if (charIndex[s.charCodeAt(right)] >= left) {
+            left = charIndex[s.charCodeAt(right)] + 1;
+        }
+        charIndex[s.charCodeAt(right)] = right;
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
-
-
